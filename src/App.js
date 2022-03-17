e07d499c248d8df52b393c0bc3b8e1169fc96fc1
yusufuysal <yusuf.uysal@agu.edu.tr>
2022-03-18 02:40:53 +0300

    Add budgets container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Stack } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button"
+
 function App() {
-  return "hey";
+  return <Container className="my-4">
+    <Stack direction="horizontal" gap="2" className="mb-4">
+      <h1 className="me-auto">Budgets</h1>
+      <Button variant="primary">Add Budget</Button>
+      <Button variant="outline-primary">Add Expense</Button>
+    </Stack>
+  </Container>
 }
 
 export default App;
