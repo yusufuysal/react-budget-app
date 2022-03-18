12b5b268d49ae3a32876a0326d412f9f4e4d389c
yusufuysal <yusuf.uysal@agu.edu.tr>
2022-03-18 03:25:16 +0300

    Add a sample budget card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Stack } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button"
+import "./App.css"
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
   return <Container className="my-4">
@@ -9,6 +11,9 @@ function App() {
       <Button variant="primary">Add Budget</Button>
       <Button variant="outline-primary">Add Expense</Button>
     </Stack>
+    <div className="budget-card-container">
+        <BudgetCard name="Entertainment" gray amount={200} max={1000}></BudgetCard>
+    </div>
   </Container>
 }
 
